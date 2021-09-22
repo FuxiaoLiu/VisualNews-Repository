41d6c6dc2be45d984cd436c749fb43e9c181c69a
FuxiaoLiu <60458715+FuxiaoLiu@users.noreply.github.com>
2021-09-22 15:39:22 -0400

    Update clean.py

--- scripts/clean.py
+++ scripts/clean.py
@@ -2,7 +2,12 @@
 #This commend can solve "UnicodeEncodeError" error.
 from bs4 import BeautifulSoup
 
+
 html = '<p>PHOENIX — Arizona state officials have paid $3 million to settle a lawsuit from a former <a href="http://azc.cc/1OHLzX7">teacher who was raped</a> and beaten by an inmate in January 2014, records obtained Monday by <em>The Arizona Republic</em> show.</p>'
 soup = BeautifulSoup(html, 'html.parser')
 result = soup.get_text()
 #result: PHOENIX — Arizona state officials have paid $3 million to settle a lawsuit from a former teacher who was raped and beaten by an inmate in January 2014, records obtained Monday by The Arizona Republic show.
+
+import re
+s = re.sub(r'[^\w\s]','',caption)
+#filter punctuations
